b215b4d55d679d8a9d739acbb3f2678b53a325d7
Eric <103733451+ewthea@users.noreply.github.com>
2023-04-14 11:44:43 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,32 @@
+x = [2.0, 3.0, -1.0]
+n = MLP (3, [4, 4, 1])
+n(x)
+
+xs = [
+    [2.0, 3.0, -1.0],
+    [3.0, -1.0, 0.5],
+    [0.5, 1.0, 1.0],
+    [1.0, 1.0, -1.0],
+    ]
+
+ys = [1.0, -1.0, -1.0, 1.0] #desired targets
+
+
+for k in range(20):
+
+    #forward pass
+    ypred = [n(x) for x in xs]
+    loss = sum((yout - ygt)*(yout - ygt) for yout, ygt in zip(ys, ypred))
+
+    #backward pass
+    for p in n.parameters():
+        p.grad = 0.0
+    loss.backward()
+    
+    #update
+    for p in n.parameters():
+        p.data += -0.1* p.grad
+
+    print(k, loss.data)
+
+    
